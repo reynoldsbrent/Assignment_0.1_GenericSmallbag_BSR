f20c366b69e09da7b50b1dae71efb71d625889f4
brentreynolds <bsreynol@iu.edu>
2022-08-31 23:33:47 -0400

    Finished most of Application class, Item class, and SmallBag class. Also added some comments

--- src/SmallBag.java
+++ src/SmallBag.java
@@ -1,10 +1,12 @@
 public class SmallBag <E>{
     E x;
 
+    //setter for Smallbag
     public void setX(E x){
         this.x = x;
     }
 
+    //getter for Smallbag
     public E getX (){
         return x;
     }
